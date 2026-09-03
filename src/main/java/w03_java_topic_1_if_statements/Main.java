90de80126a3fba4b4cc8bf7437072baab0bfccb5
Tanya <tanya.griz89@gmail.com>
2026-09-03 13:43:38 -0500

    added more practice

--- src/main/java/w03_java_topic_1_if_statements/Main.java
+++ src/main/java/w03_java_topic_1_if_statements/Main.java
@@ -1,14 +1,38 @@
 package w03_java_topic_1_if_statements;
 
-import static input.InputUtils.stringInput;
+import static input.InputUtils.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Welcome a user
-        String name = stringInput("Please enter your name and press enter:");
-        System.out.println("Nice to meet you, " + name);
+        System.out.println("Enter today`s temperature in C");
+        double temp = doubleInput();
+
+        if (temp < 15) {
+            System.out.println("Cold");
+        } else {
+            System.out.println("Good");
+        }
+
+        //
+        double miles = doubleInput("How many miles do you live from MCTC? ");
+
+        if (miles > 10) {
+            System.out.println("You live more than 10 miles from MCTC");
+        } else if (miles == 10) {
+            System.out.println("You live exactly 10 miles from MCTC");
+        } else {
+            System.out.println("You live less than 10 miles from MCTC");
+        }
+
+        //
+        double temp2 = doubleInput("What is the temperature?");
+        int rain = intInput("How much rain? Enter 0 if there is no rain:");
+
+        if (temp2 > 70 && rain == 0) {
+            System.out.println("Warm and no rain - a nice day!");
+        }
     }
 
 }
